74dad0ebb7e0aea3fde3dbced2afb743075f8957
Luke Herrmann <herrmannluke@gmail.com>
2021-02-16 22:13:09 -0700

    default props practice

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import Movie from './Movie'
 const movies = [
   {
     id: 1,
-    title: `Star Wars`,
+    title: 'Star Wars',
+    desc: 'A Space Movie'
   },
   {
     id: 2,
-    title: `Spiderman`,
+    title: 'Spiderman',
   },
   {
     id: 3,
-    title: `Harry Potter`,
+    title: 'Harry Potter',
   },
 ];
 
@@ -27,7 +28,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         {movies.map((movie) => (
-          <Movie key={movie.id} movie={movie} />
+          <Movie key={movie.id} movie={movie} desc={movie.desc} />
         ))}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -4,14 +4,21 @@ import PropTypes from "prop-types";
 
 export default class Movie extends Component {
   static propTypes = {
-    movie: PropTypes.string
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  }
+
+  static defaultProps = {
+    desc: 'Description no available'
   }
 
   render() {
     return(
       <div>
         <h3>{this.props.movie.title}</h3>
-        <p>{this.props.movie.desc}</p>
+        <p>{this.props.desc}</p>
       </div>
     )
   }
